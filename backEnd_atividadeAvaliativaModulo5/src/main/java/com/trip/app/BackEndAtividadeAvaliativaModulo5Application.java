16ebb5c7dfd36dc0799d993161af5e0b63fbf530
Samuel Rezende <sam_profissional@outlook.com>
2022-11-09 11:25:14 -0300

    atividade avaliativa mod.5 RecodePRO_v1.2.2

--- backEnd_atividadeAvaliativaModulo5/src/main/java/com/trip/app/BackEndAtividadeAvaliativaModulo5Application.java
+++ backEnd_atividadeAvaliativaModulo5/src/main/java/com/trip/app/BackEndAtividadeAvaliativaModulo5Application.java
@@ -6,14 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan(basePackages = {"com.trip.model.entity"})
-@EnableJpaRepositories(basePackages = {"com.trip.repositories"})
-@ComponentScan(basePackages = {"com.trip.controller", "com.trip.services", "com.trip.services.impl"})
+@EntityScan(basePackages = { "com.trip.model.entity" })
+@EnableJpaRepositories(basePackages = { "com.trip.repositories" })
+@ComponentScan(basePackages = { "com.trip.controller", "com.trip.services", "com.trip.services.impl" })
 @SpringBootApplication
-public class BackEndAtividadeAvaliativaModulo4Application {
+public class BackEndAtividadeAvaliativaModulo5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackEndAtividadeAvaliativaModulo4Application.class, args);
+		SpringApplication.run(BackEndAtividadeAvaliativaModulo5Application.class, args);
 	}
 
 }
--- backEnd_atividadeAvaliativaModulo5/src/test/java/com/trip/app/BackEndAtividadeAvaliativaModulo5ApplicationTests.java
+++ backEnd_atividadeAvaliativaModulo5/src/test/java/com/trip/app/BackEndAtividadeAvaliativaModulo5ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackEndAtividadeAvaliativaModulo4ApplicationTests {
+class BackEndAtividadeAvaliativaModulo5ApplicationTests {
 
 	@Test
 	void contextLoads() {
